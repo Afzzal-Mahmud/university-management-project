9a8cb9008b6f5d96131960c5e5eb278aa00e5ae0
afzal mahmud <afzzalmahmud@gmail.com>
2023-06-15 17:37:08 +0600

    [validation] detarmined code for each semester

--- src/app/modules/academicSemester/academicSemester.service.query.ts
+++ src/app/modules/academicSemester/academicSemester.service.query.ts
@@ -1,9 +1,17 @@
+import ApiErrors from '../../../errors/ApiErrors'
+import { academicSemesterCodeMapper } from './academicSemester.constant'
 import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemester } from './academicSemester.model'
 
 const createAcademicSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterCodeMapper[payload.title] !== payload.code) {
+    throw new ApiErrors(
+      400,
+      "Invalid Semester Code it should be like '01' for 'Autumn', '02' for 'summer' and 03 for Fall semester"
+    )
+  }
   const result = await AcademicSemester.create(payload)
   return result
 }
