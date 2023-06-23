86a995cbeb0a153c728cf4da95206efbbc5b8b34
afzal mahmud <afzzalmahmud@gmail.com>
2023-06-23 18:06:11 +0600

    [Modified,Test] improvement of student id and test successfull

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -1,19 +1,25 @@
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface'
 import { User } from './users.model'
 
-export async function generateUserId(): Promise<string> {
+export async function generateStudentId(
+  academicSemester: IAcademicSemester
+): Promise<string> {
   // Retrieve the last created user from the database
-  const lastUserId = await User.findOne({}, { id: 1, _id: 0 })
+  const lastStudentId = await User.findOne({}, { id: 1, _id: 0 })
     .sort({ id: -1 })
     .lean()
 
   // Determine the current highest user ID
-  const highestId = lastUserId ? parseInt(lastUserId.id.slice(2)) : 0
+  const highestId = lastStudentId ? parseInt(lastStudentId.id.slice(6)) : 0
 
-  // Increment the highest user ID by 1 and format as 5-digit string with leading zeroes
-  const nextId = (highestId + 1).toString().padStart(5, '0')
+  // Increment the highest user ID by 1 and format as 5-digit string with leading zeroes and also addint prefix before year as Y and which intake such as summer, auttum, fall as I
+  let nextId = (highestId + 1).toString().padStart(5, '0')
 
-  // Generate the final user ID with the "UW" prefix
-  const userId = `UW${nextId}`
+  nextId = `Y${academicSemester.year.substring(2)}I${
+    academicSemester.code
+  }${nextId}`
 
-  return userId
+  const studentId = nextId
+
+  return studentId
 }
